78f5b7b748421188fc3f8c25cd4302b7f0bf8157
nimrag-b <124409677+nimrag-b@users.noreply.github.com>
2024-11-11 05:12:17 +0000

    added example file

--- emiT C/Program.cs
+++ emiT C/Program.cs
@@ -1,36 +1,25 @@
 ﻿using emiT_C;
+using System.IO;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+
+        string src;
+        if (args.Length == 0)
+        {
+            string exefolder = System.Reflection.Assembly.GetEntryAssembly().Location;
+            src = File.ReadAllText(Path.GetFullPath(Path.Combine(exefolder, "..", "..", "..", "..","..","examples","example.emit")));
+        }
+        else
+        {
+            src = File.ReadAllText(args[0]);  
+        }
+
         Console.WriteLine("Starting Primary Timeline...");
         Lexer lexer = new Lexer();
-        List<Token> tokens = lexer.Tokenize("""
-            create x = 10;
-
-            x = x - 3;
-
-            create y;
-
-            time strike;
-
-            y kills x;
-
-            create traveler;
-
-            print(x); //timeline 1 - nothing since x is dead. timeline 2 - prints 7 since traveler stopped x from being killed.
-
-            if(x is dead){
-
-                traveler warps strike{
-                    //traveler waits strike;
-                    traveler kills y;
-                    traveler kills traveler; //kill self, preventing paradoxes by interfering with itself
-                };
-
-            };
-            """);
+        List<Token> tokens = lexer.Tokenize(src);
 
         //foreach (var item in tokens)
         //{
